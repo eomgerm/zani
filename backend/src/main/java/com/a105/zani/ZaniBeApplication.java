d7c9f02a98880405c9430994542995166614114a
eomgerm <rlgns0705@naver.com>
2026-07-19 21:50:25 +0900

    🎉 init: 백엔드 프로젝트 초기 커밋 (S15P11A105-147)

---
+++ backend/src/main/java/com/a105/zani/ZaniBeApplication.java
@@ -0,0 +1,13 @@
+package com.a105.zani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaniBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaniBeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/a105/zani/ZaniBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a105.zani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaniBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
